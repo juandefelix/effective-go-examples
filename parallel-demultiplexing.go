b30b04af7ef3006ff745ec681dcc755779c15660
Juan Ortiz <juanfortizd@gmail.com>
2024-02-09 17:50:51 -0600

    change type of message

--- parallel-demultiplexing.go
+++ parallel-demultiplexing.go
@@ -3,39 +3,27 @@ package main
 import "fmt"
 
 // functions that post to different social media
-func postToInstagram([]byte) bool {
+func postToInstagram(text string) bool {
 	fmt.Println("Posted to Instagram successfully")
 	return true
 }
 
-func postToTikTok([]byte) bool {
+func postToTikTok(text string) bool {
 	fmt.Println("Posted to TikTok successfully")
 	return true
 }
 
 type SocialMediaPostRequest struct {
 	text string
-	function func([]byte) bool
-	notifyChannel chan bool
+	function func(string) bool
+	// notifyChannel chan int
 }
 
-func prepareRequests() chan SocialMediaPostRequest {
-	clientRequests := make(chan SocialMediaPostRequest)
-
-	instagramRequestSuccessChannel := make(chan bool)
-	tikTokRequestSuccessChannel := make(chan bool)
-
-	instagramPost := SocialMediaPostRequest{"Hello Instagram!", postToInstagram, instagramRequestSuccessChannel}
-	tikTokPost := SocialMediaPostRequest{"Hello TikTok!", postToTikTok, tikTokRequestSuccessChannel}
-
+func sendRequests(clientRequests chan *SocialMediaPostRequest) {
+	instagramPost := &SocialMediaPostRequest{"Hello Instagram!", postToInstagram}
+	tikTokPost := &SocialMediaPostRequest{"Hello TikTok!", postToTikTok}
 	clientRequests <- instagramPost
 	clientRequests <- tikTokPost
-
-	return clientRequests
-}
-
-func sendRequests(clientRequests chan SocialMediaPostRequest) {
-	// TODO
 }
 
 func main() {
